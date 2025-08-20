6f740ca63ec35dbe9e19fbc8519740402f7b4ded
maximomp <titusoog@gmail.com>
2025-08-20 08:12:32 +0200

    ejercicio2.js

---
+++ ejercicio2.js
@@ -0,0 +1,6 @@
+//Ejercicio 2
+//1. Luke Skywalker cumple años:
+//Modifica el siguiente código JavaScript para actualizar la edad de Luke Skywalker a 25 años.
+const jedi = { nombre: "Luke Skywalker", edad: 19 };
+jedi.edad = 25;
+console.log(jedi);
