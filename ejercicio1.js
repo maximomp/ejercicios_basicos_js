a306ea9290228e00d59970e612ed7d318e3f57d4
maximomp <titusoog@gmail.com>
2025-08-20 08:08:56 +0200

    first commit

---
+++ ejercicio1.js
@@ -0,0 +1,16 @@
+// Ejercicio 1
+// 1. Define tu superhéroe favorito:
+let miSuperheroeFavorito = "Hulk";
+// 2. Define un número:
+let numeroFavorito = 50;
+// 3. Define las dimensiones:
+let altura = 5;
+let longitud = 10;
+// 4. Calcula la suma:
+let suma = altura + longitud;
+// 5. Imprime los resultados:
+console.log("miSuperheroeFavorito:", miSuperheroeFavorito);
+console.log("numeroFavorito:", numeroFavorito);
+console.log("altura:", altura);
+console.log("longitud:", longitud);
+console.log("suma:", suma);
